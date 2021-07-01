6376d95a7b47348b14e53de453178ecaf434f9d2
Adam <adamprodev@gmail.com>
2021-07-01 09:02:29 +0300

    for Sapir

---
+++ src/test/java/com/bootsnipp/dev/SapirSolutions.java
@@ -0,0 +1,14 @@
+package com.bootsnipp.dev;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class SapirSolutions {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sapirSolutions/ford_escort.csv", numLinesToSkip = 0)
+    void runTestOnCsvData(String year, String mileage, String price){
+
+        System.out.println(year + ", " + mileage + ", " + price);
+    }
+}
